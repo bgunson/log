34d9e8262278e78c5b1935e2df395e11753d919c
Bennett Gunson <bengunson@gmail.com>
2021-01-08 16:34:34 -0700

    starting auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { MatIconModule } from '@angular/material/icon';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { NgxAuthFirebaseUIModule } from 'ngx-auth-firebaseui';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,36 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
     MatToolbarModule,
     MatIconModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    // Specify the ngx-auth-firebaseui library as an import
+    NgxAuthFirebaseUIModule.forRoot(
+      {
+        apiKey: 'your-firebase-apiKey',
+        authDomain: 'your-firebase-authDomain',
+        databaseURL: 'your-firebase-databaseURL',
+        projectId: 'your-firebase-projectId',
+        storageBucket: 'your-firebase-storageBucket',
+        messagingSenderId: 'your-firebase-messagingSenderId'
+      },
+       () => 'your_app_name_factory',
+      {
+        enableFirestoreSync: true, // enable/disable autosync users with firestore
+        toastMessageOnAuthSuccess: false, // whether to open/show a snackbar message on auth success - default : true
+        toastMessageOnAuthError: false, // whether to open/show a snackbar message on auth error - default : true
+        authGuardFallbackURL: '/loggedout', // url for unauthenticated users - to use in combination with canActivate feature on a route
+        authGuardLoggedInURL: '/loggedin', // url for authenticated users - to use in combination with canActivate feature on a route
+        passwordMaxLength: 60, // `min/max` input parameters in components should be within this range.
+        passwordMinLength: 8, // Password length min/max in forms independently of each componenet min/max.
+        // Same as password but for the name
+        nameMaxLength: 50,
+        nameMinLength: 2,
+        // If set, sign-in/up form is not available until email has been verified.
+        // Plus protected routes are still protected even though user is connected.
+        guardProtectedRoutesUntilEmailIsVerified: true,
+        enableEmailVerification: true, // default: true
+      }),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
