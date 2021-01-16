c4571e1f895a6c83d4b0d69a9f773525f9b3d158
Bennett Gunson <bengunson@gmail.com>
2021-01-15 17:32:33 -0700

    new login/register splash pages

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { AuthService } from '../services/auth.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
@@ -12,16 +11,13 @@ import { AngularFireAuth } from '@angular/fire/auth';
 })
 export class SidebarComponent {
 
-
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver, public auth: AngularFireAuth) {
-      
-  
-  }
+  constructor(private breakpointObserver: BreakpointObserver, public auth: AngularFireAuth) { }
+
 
 }
